9c57b176a5351dc7e7094a52eb0c60785f932c61
Anton Orlichenko <aorliche@gmail.com>
2023-05-21 00:15:11 -0500

    Found enumeration up to N=7

--- enumerate.js
+++ enumerate.js
@@ -1,6 +1,74 @@
 
+export {triplets, getFlips, enumerate, cullTriplets, mspChainsToRaw};
+
+function chainsEq(c1, c2) {
+    if (c1.length != c2.length) return false;
+    for (let i=0; i<c1.length; i++) {
+        if (c1[i].length != c2[i].length) return false;
+        for (let j=0; j<c1[i].length; j++) {
+            if (c1[i][j] != c2[i][j]) return false;
+        }
+    }
+    return true;
+}
+
+function hasVisited(db, chains) {
+    for (let i=0; i<db.length; i++) {
+        if (chainsEq(db[i], chains)) return true;
+    }
+    return false;
+}
+
+function cullTriplets(chains, trips) {
+    return trips.filter(trip => {
+        let [ab, ac, bc] = [false, false, false];
+        chains.forEach(c => {
+            if (c.includes(trip[0]) && c.includes(trip[1])) ab = true;
+            if (c.includes(trip[0]) && c.includes(trip[2])) ac = true;
+            if (c.includes(trip[1]) && c.includes(trip[2])) bc = true;
+        });
+        return ab && ac && bc;
+    });
+}
+
 function enumerate(chains, rhombi) {
-    
+    const trips = cullTriplets(chains, triplets(rhombi));
+    const db = [chains];
+    let edge = [chains];
+    let iter = 0;
+    while (edge.length > 0) {
+        const newEdge = [];
+        edge.forEach(cs => {
+            const flips = getFlips(cs, trips);
+            flips.forEach(trip => {
+                const cloneCs = cloneChains(cs);
+                flip(cloneCs, trip)
+                if (!hasVisited(db, cloneCs)) {
+                    db.push(cloneCs);
+                    newEdge.push(cloneCs);
+                }
+            });
+        });
+        edge = newEdge;
+        if (iter++ > 50) throw 'Exceeded iters';
+    }
+    return db;
+}
+
+function mspChainsToRaw(chains) {
+    const newChains = [];
+    chains.forEach(c => {
+        newChains.push(c.rhombi.map(ridx => ridx));
+    });
+    return newChains;
+}
+
+function cloneChains(chains) {
+    const newChains = [];
+    chains.forEach(c => {
+        newChains.push(c.map(r => r));
+    });
+    return newChains;
 }
 
 function triplets(rhombi) {
@@ -13,4 +81,43 @@ function triplets(rhombi) {
         }
     }
     return trips;
+}
+
+function inTrip(ch, i, a, b, c) {
+    if (ch[i] == a && ch[i+1] == b) return 1;
+    if (ch[i] == b && ch[i+1] == a) return 1;
+    if (ch[i] == a && ch[i+1] == c) return 2;
+    if (ch[i] == c && ch[i+1] == a) return 2;
+    if (ch[i] == b && ch[i+1] == c) return 3;
+    if (ch[i] == c && ch[i+1] == b) return 3;
+    return 0;
+}
+
+function getFlips(chains, trips) {
+    const flips = [];
+    trips.forEach(trip => {
+        let [ab, ac, bc] = [false, false, false];
+        chains.forEach(c => {
+            for (let i=0; i<c.length-1; i++) {
+                const pair = inTrip(c, i, trip[0], trip[1], trip[2]);
+                switch (pair) {
+                    case 1: ab = true; break;
+                    case 2: ac = true; break;
+                    case 3: bc = true; break;
+                }
+            }
+        });
+        if (ab && ac && bc) flips.push(trip);
+    });
+    return flips;
+}
+
+function flip(chains, trip) {
+    chains.forEach(c => {
+        for (let i=0; i<c.length-1; i++) {
+            if (inTrip(c, i, trip[0], trip[1], trip[2]) != 0) {
+                [c[i], c[i+1]] = [c[i+1], c[i]];
+            }
+        }
+    });
 }
